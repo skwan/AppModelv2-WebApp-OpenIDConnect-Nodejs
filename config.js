57165b95070995669452956f37bd1e19d48405e9
skwan <skwan@microsoft.com>
2015-08-31 11:09:30 -0700

    Update metadata endpoint to B2C

--- config.js
+++ config.js
@@ -2,7 +2,10 @@
   //
  exports.creds = {
  	returnURL: 'https://skwantoso-saml-test.cloudapp.net/auth/openid/return',
- 	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+ 	// MS STS
+	// identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', 
+	// AAD B2C 
+	identityMetadata: 'https://login.microsoftonline.com/skwantosob2ctest.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_DefaultSignIn',
  	clientID: 'cc7d475b-0c4b-43c3-b9c8-5ee08928aadf',
  	clientSecret: 'CEzV4w0uW7uzjVXDPoA2ugp',
  	skipUserProfile: true, // for OpenID only flows this should be set to true
