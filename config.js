51b4437dc97c00535aed96a2103bdf8038db210c
skwan <skwan@microsoft.com>
2015-08-31 18:01:12 -0700

    New B2C version of passport-azure-ad

--- config.js
+++ config.js
@@ -2,13 +2,7 @@
   //
  exports.creds = {
  	returnURL: 'https://skwantoso-saml-test.cloudapp.net/auth/openid/return',
- 	// MS STS
-	//
-	// identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-	// 
-	// AAD B2C
-	// 
-	identityMetadata: 'https://login.microsoftonline.com/skwantosob2ctest.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_DefaultSignIn',
+	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
  	// MS STS
 	// clientID: 'cc7d475b-0c4b-43c3-b9c8-5ee08928aadf',
  	// clientSecret: 'CEzV4w0uW7uzjVXDPoA2ugp',
@@ -20,6 +14,7 @@
  	skipUserProfile: true, // for OpenID only flows this should be set to true
  	responseType: 'id_token', // for login only flows
  	responseMode: 'form_post', // As per the OAuth 2.0 standard.
+	tenantName: 'skwantosob2ctest.onmicrosoft.com'
  	//scope: ['email', 'profile'] // additional scopes you may wish to pass
 
  };
